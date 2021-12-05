028cb4799a386660efb9376e74c200be21197a11
minsupark <erjuer01@gmail.com>
2021-12-05 14:30:28 +0900

    LeetCode: House Robber

--- leetcode/House Robber.cpp
+++ leetcode/House Robber.cpp
@@ -2,7 +2,7 @@ class Solution {
     
 public:
     int rob(vector<int>& nums) {
-     
+
         int maxArraySize = nums.size();
     
         int* maxNum = new int[maxArraySize];
@@ -14,7 +14,7 @@ public:
         if(nums.size() < 3) return max(maxNum[0], maxNum[1]); // size가 2개일 때 0번째 또는 1번째 Index 중 큰 값이 max값이다.
         
         for(int i=2; i<nums.size(); i++) {
-             
+
             maxNum[i] = max(maxNum[i-2]+nums[i], maxNum[i-1]);
         }
         
